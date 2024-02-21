fe12f24a537c7d57d2450efcf7e09fe9919dce53
anikav <anikav@10.232.160.150>
2024-02-21 14:00:51 -0600

    drawing and flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,14 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(Color.GREEN);   
+		StdDraw.filledRectangle(.165, 0.5, 0.125, 0.375);
+		StdDraw.setPenColor(Color.blue);   
+		StdDraw.filledRectangle(.40, 0.5, 0.125, 0.375);
+		StdDraw.setPenColor(Color.red);   
+		StdDraw.filledRectangle(.65, 0.5, 0.125, 0.375); 
+		StdDraw.setPenColor(Color.cyan);   
+		StdDraw.filledRectangle(0.9, 0.5, 0.125, 0.375);
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,18 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shapeType = in.nextLine(); 
+		int redComponent = in.nextInt(); 
+		int greenComponent = in.nextInt(); 
+		int blueComponent = in.nextInt(); 
+		boolean isFillied = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double halfWidth = in.nextDouble();
+		double halfHeigth= in.nextDouble();
+		StdDraw.setPenColor(Color.GREEN);  
 		
-	}
+
+		
+    }	
 }
